4d9333e9f9d1bad3921f8e617ee8866e9ce23c4f
Francisco Gorina Vanrell <fgorina@gmail.com>
2023-10-24 17:17:45 +0200

    Initial Commit

---
+++ Molinet K/Molinet_KApp.swift
@@ -0,0 +1,17 @@
+//
+//  Molinet_KApp.swift
+//  Molinet K
+//
+//  Created by Francisco Gorina Vanrell on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Molinet_KApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
